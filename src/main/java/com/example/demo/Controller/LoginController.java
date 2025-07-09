b88c4e5304563eedbb4f4e1324f3a1106288055a
myapp <kaillcgc@gmail.com>
2025-07-10 00:23:18 +0800

    myapphd

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+//@RequestMapping("/api/auth")
 public class LoginController {
     @Autowired
     private UserServices userServices;
